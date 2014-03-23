2ff2c322f99be62d72aa73b55e260c79baa4782b
AJ Bahnken <aj@ajvb.me>
2014-03-22 17:27:48 -0700

    Fixed the paths to work with /ui/app

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -9,15 +9,15 @@ angular.module('uiApp', [
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: '/media/ui/app/views/main.html',
+        templateUrl: '/ui/app/views/main.html',
         controller: 'MainCtrl'
       })
       .when('/tasks', {
-        templateUrl: '/media/ui/app/views/tasks.html',
+        templateUrl: '/ui/app/views/tasks.html',
         controller: 'TasksCtrl'
       })
       .when('/calendar', {
-        templateUrl: '/media/ui/app/views/calendar.html',
+        templateUrl: '/ui/app/views/calendar.html',
         controller: 'CalendarCtrl'
       })
       .otherwise({
