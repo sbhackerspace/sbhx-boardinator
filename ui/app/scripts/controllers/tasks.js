d02a89710815fac0f9f9435aa127dd296abe7478
jaykan <leicasper@gmail.com>
2014-03-26 23:18:06 -0700

    Add task table to display sample tasks data. Edit task from to allow create New Task to display on task table.

--- ui/app/scripts/controllers/tasks.js
+++ ui/app/scripts/controllers/tasks.js
@@ -1,30 +1,41 @@
 'use strict';
 
-app.controller('TasksCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+app.controller('TasksCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
 
     $scope.showTaskList = true;
+    $scope.tasks = [
+        {assignor: 'Jay Kan', name: 'Task Name', dueDate: '12/31/2015', description: 'Sample Task Description Sample Task Description Sample Task Description', priority: 'High'},
+        {assignor: 'AJ', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Sample Task Description Description', priority: 'Medium'},
+        {assignor: 'Steve Phillips', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Task Description Sample Task Description', priority: 'Low'},
+        {assignor: 'Jim', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Task Description Sample Task Description', priority: 'Medium'},
+        {assignor: 'Garry', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Task Description Sample Task Description', priority: 'Low'},
+        {assignor: 'Whatever', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Task DescriptionSample Task Description', priority: 'High'},
+        {assignor: 'ABC', name: 'Task Name', dueDate: '12/31/2015', description: 'Test Task Sample Task Description Description', priority: 'High'},
+    ];
+    $scope.submitTaskForm = function(formData) {         
+        // Format Date 
+        var datefilter = $filter('date'),
+            formattedDate = datefilter(formData.dueDate, 'yyyy/MM/dd');   
+        // API call            
+        // var params = {            
+        //     Name: formData.name,
+        //     Description: formData.description,
+        //     DueDate: formData.dueDate,
+        //     Assignee: formData.assignee,
+        // };
 
-    $scope.submitTaskForm = function (fromData) {
-        var params = {        
-            Name: formData.name,
-            Description: formData.description,
-            DueDate: formData.dueDate,
-            Assignee: formData.assignee,
-        };
+        // $http.post('url', params)
+        //     .then(function(e) {
 
-        $http.post('url', params)
-            .then(function(e) {                
-            })      
+        //     })                 
+        $scope.tasks.push(formData);
     };
    
     $scope.dateOptions = {
-        'year-format': "'yy'",
+        'year-format': "'yyyy'",
         'starting-day': 1
-    };
+    };    
+    $scope.formats = ['MM/dd/yyyy', 'yyyy/MM/dd', 'shortDate'];
+    $scope.format = $scope.formats[1];
 
   }]);
